edddf7a449caf773ff6d6ec28010a4ad2d76c43b
mattia46old <mattiacolalongo82@gmail.com>
2024-12-11 16:09:55 +0100

    viz echem data

---
+++ readclass.py
@@ -0,0 +1,20 @@
+# no need for shebang as the class is imported in jupyternotebook and mypyvenv is selected afterwards. All good
+
+import pandas as pd #lib for database management
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import numpy as np
+
+class Echem:
+
+    def __init__(self, filepath, min_cycle, max_cycle):
+        self.f = filepath # the constructor stores the filepath
+        self.cmin = min_cycle
+        self.cmax = max_cycle + 1
+        self.range = np.arange(self.cmin, self.cmax)
+
+
+
+    def extractData(self):
+        self.df = pd.read_csv(self.f, sep=',')
+        return self.df
