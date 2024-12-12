b5cd4d7acdf5b3a5edc8cc9af2335e1c33ce0fe9
mattia46old <mattiacolalongo82@gmail.com>
2024-12-12 17:50:17 +0100

    final version?

--- readclass.py
+++ readclass.py
@@ -16,7 +16,8 @@ class Echem:
 
     def plotDqDv(self):
         self.fig, self.ax = plt.subplots(dpi=100, figsize=(4,3)) 
-        for j in self.range:
+        for j in self.range:        # self.color = cm.Blues_r(np.linspace(0, 1, len(self.ncycl)+2))
+
             for i in os.listdir(self.f):
                 if (f"cycle_{j}.csv") in i:
                     self.df = pd.read_csv(os.path.join(self.f, i), sep=',')
@@ -24,13 +25,20 @@ class Echem:
         # plt.show()
         # return self.df
     
-    def plotDqDvFilter(self):
-        self.ncycl = [6, 21]
-        self.color = cm.jet(np.linspace(0, 1, len(self.ncycl)))
+    def plotDqDvFilter(self, color):
+        '''
+        color as string. i.e. "jet", "turbo", "Blues_r" .... 
+        '''
+        self.dic = {}
+        self.ncycl = [6, 22]
+        # self.color = cm.Blues_r(np.linspace(0, 1, len(self.ncycl)+2))
+        self.cmap = plt.colormaps[color]
+        self.color = self.cmap(np.linspace(0,1,3)) # init, end, ncolors
         self.fig, self.ax = plt.subplots(dpi=100) 
         for n,j in enumerate(self.ncycl):
             for i in os.listdir(self.f):
-                if (f"cycle_{j}.csv") in i:
+                if (f"cycle_{j}.csv") in i: 
                     self.df = pd.read_csv(os.path.join(self.f, i), sep=',')
+                    self.dic[f"cycle_{j}"] = self.df
                     self.ax.plot(self.df['E_V'], self.df['dCap_mAh_dE_V'], color=self.color[n])
-        # plt.show()
+        return self.dic
